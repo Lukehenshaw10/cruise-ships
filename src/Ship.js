9d85ba5b05f7aebd64f4e4c081dd316a874514ca
Luke Henshaw <lukehenshaw10@gmail.com>
2022-06-23 18:58:10 +0100

    Ship can set sail

--- src/Ship.js
+++ src/Ship.js
@@ -1,4 +1,11 @@
-function Ship() {
-    this.startingPort = 'Dover';
-};
-    module.exports = Ship;
+function Ship(port) {
+    this.startingPort = port;
+  }
+  
+  Ship.prototype = {
+    setSail() {
+      this.startingPort = null;
+    },
+  };
+  
+  module.exports = Ship;
